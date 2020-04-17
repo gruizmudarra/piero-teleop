689ec0330a5491eab110146eb988a36eec8d8ff8
gruizmudarra <grmanyt@gmail.com>
2020-04-17 17:04:19 +0200

    ESP8266 program updated

--- esp_server/esp_server.ino
+++ esp_server/esp_server.ino
@@ -1,59 +1,47 @@
-/*
-  LIBRARIES
-*/
 #include <ESP8266WiFi.h>
-
-/*
-  GLOBAL VARIABLES
-*/
-
+// GLOBAL VARIABLES
 WiFiServer wifiServer(80); // Server instance
 const char* ssid = "MOVISTAR_48BA";
 const char* password = "2094B0CEBB5AB5D2DF58";
-
-
 // Set your Static IP address
 IPAddress local_IP(192, 168, 1, 131);
 // Set your Gateway IP address
 IPAddress gateway(192, 168, 1, 1);
-
+// Set your subnet mask
 IPAddress subnet(255, 255, 0, 0);
-/*
-  CUSTOM FUNCTIONS
-*/
 
-// Setup wifi connection
-void setup_wifi() {
+// CUSTOM FUNCTIONS
+void setup_wifi() { // Setup wifi connection
   // Configures static IP address
   if (!WiFi.config(local_IP, gateway, subnet)) {
-    //Serial.println("STA Failed to configure");
+    /* Serial.println("STA Failed to configure"); */
   }
-
   // Connect to Wi-Fi network with SSID and password
-  //Serial.print("Connecting to ");
-  //Serial.println(ssid);
+  /*
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+  */
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   // Print local IP address and start web server
-  //Serial.println("");
-  //Serial.println("WiFi connected.");
-  //Serial.println("IP address: ");
-  //Serial.println(WiFi.localIP());
-  //Serial.println(WiFi.macAddress());
-  digitalWrite(LED_BUILTIN,HIGH);
+  /*
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.macAddress());
+    digitalWrite(LED_BUILTIN,HIGH);
+  */
   wifiServer.begin();
 }
 
-/*
-  INITIAL SETUP AND MAIN LOOP
-*/
-
+// INITIAL SETUP AND MAIN LOOP
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   delay(1000);
   setup_wifi();
 }
@@ -62,45 +50,28 @@ void loop() {
   int l = 0;
   int a = 0;
   String command = ""; // Command received from the app
-  bool firstValue = true;
   WiFiClient app = wifiServer.available();
-
   if (app) {
     //Serial.println("Client connected");
-    while (app.connected()) { // While the client is connected (only one client permitted) 
-      /***************
-       * PARSING MSG * 
-       ***************/
-       
-      while (app.available() > 0) {
+    while (app.connected()) { // While the client is connected (only one client permitted)
+      while (app.available() > 0) { // While the client speaks
         char c = app.read();
-        Serial.write(c);
-        if (c == '\n') {
-          if(firstValue) { 
-            l = command.toInt();            // casting and saving the command string to a double variable
-            firstValue = false;
-            command = "";
-          }
-          else {
-              a = command.toInt();          // casting and saving the command string to a double variable
-              firstValue = true;
-//              Serial.printf("Linear: %d\tAngular: %d\n",l,a);
-              break;
-            }
+        Serial.write(c); // Send caracther through serial connection as it comes
+        if (c == 'l') {
+          l = command.toInt();    // casting and saving the command string to an integer variable
+          /* Serial.printf("Linear: %d\tAngular: %d\n", l, a); */ // Debug
+          command = "";
         }
-        else{
-            command += c;                   // concatenate the received characters to form a string
-          }
+        if (c == 'a') {
+          a = command.toInt();    // casting and saving the command string to an integer variable
+          /* Serial.printf("Linear: %d\tAngular: %d\n", l, a); */ // Debug
+          command = "";
+        }
+        command += c;   // concatenate the received characters to form a string
       }
-      
-      command = ""; // reset string
-      delay(1);
-      
-      /*******************
-       * END PARSING MSG * 
-       *******************/
     }
-    app.stop();
-    //Serial.println("Client disconnected");
+    delay(1);
   }
+  app.stop();
+  /* Serial.println("Client disconnected"); */ // Debug
 }
