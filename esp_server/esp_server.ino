77b4a8ace2a31807cd28717bf3fc68a58a115cd9
PRono8 <peperono8@gmail.com>
2020-04-19 16:16:30 +0200

    Actualización de ESP server. Envía los caracteres cuando se han leído todos los datos

--- esp_server/esp_server.ino
+++ esp_server/esp_server.ino
@@ -40,37 +40,41 @@ void setup_wifi() { // Setup wifi connection
 
 // INITIAL SETUP AND MAIN LOOP
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
   delay(1000);
   setup_wifi();
 }
 
 void loop() {
-  int l = 0;
-  int a = 0;
   String command = ""; // Command received from the app
+  char c[4];  // Char array 
+  int i = 0; 
   WiFiClient app = wifiServer.available();
   if (app) {
     //Serial.println("Client connected");
     while (app.connected()) { // While the client is connected (only one client permitted)
       while (app.available() > 0) { // While the client speaks
-        char c = app.read();
-        Serial.write(c); // Send caracther through serial connection as it comes
-        if (c == 'l') {
-          l = command.toInt();    // casting and saving the command string to an integer variable
-          /* Serial.printf("Linear: %d\tAngular: %d\n", l, a); */ // Debug
-          command = "";
-        }
-        if (c == 'a') {
-          a = command.toInt();    // casting and saving the command string to an integer variable
-          /* Serial.printf("Linear: %d\tAngular: %d\n", l, a); */ // Debug
-          command = "";
-        }
-        command += c;   // concatenate the received characters to form a string
+        c[i] = app.read();  // Read from the app and keep in c
+          if (c[i] == '\n') {  // If the full message is readed
+            for(int y = 0; y < 4; y++) { 
+              if (c[y] != NULL){  // If the character is not NULL
+                Serial.write(c[y]); // Write the character
+                delay(50);
+              }
+            }
+            delay(4*50);
+            i = 0;  // Reset i
+            for (int j = 0; j < 4; j++){
+              c[j] = 0; // Reset c
+            }
+            break;
+          }
+          i++;
       }
+      delay(5);
     }
-    delay(1);
+    
   }
   app.stop();
   /* Serial.println("Client disconnected"); */ // Debug
